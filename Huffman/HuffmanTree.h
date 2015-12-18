7adea9912dcf22c7cb79ef0a037b483f9f1d8f3b
hang liu <hang liu>
2015-12-18 23:07:23 +0800

    initial

--- Huffman/HuffmanTree.h
+++ Huffman/HuffmanTree.h
@@ -1,2 +1,29 @@
 #include <iostream>
 using namespace std;
+
+#define maxSize 100
+
+struct TreeNode
+{
+	float weight;
+	char letter;
+	TreeNode *LeftChild,*RightChild,*parent;
+	//int LeftChild,RightChild,parent;
+}
+
+class HuffmanTree
+{
+private:
+	TreeNode ArrayTree[maxSize];
+	TreeNode ArrayTreeRest[maxSize];
+	int size;
+public:
+	HuffmanTree();
+	~HuffmanTree();
+	void Sort();   //÷±Ω”—°‘Ò≈≈–Ú
+	void CreatHuffmanTree();
+	void InsertNode();
+	void DeleteNode();
+	void Code();
+	void Decode();
+}
